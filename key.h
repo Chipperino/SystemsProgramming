c72835efdc57bfa562985e02eb5cd31bd2a2cb30
Chip Simm <buddysimm@gmail.com>
2020-03-15 15:18:03 -0400

    Update key.h

--- key.h
+++ key.h
@@ -1,26 +1,3 @@
-/* key.h ---------
- *
- * Filename: key.h
- * Description:
- * Author: Bryce Himebaugh
- * Maintainer:
- * Created: Thu Sep 15 16:36:21 2017
- * Last-Updated: September 23 15:38 2018
- *           By: Dmitrii Galantsev
- *
- */
-
-/* Commentary:
- *
- *
- *
- */
-
-/* Change log:
- *
- *
- */
-
 /* Copyright (c) 2016 The Trustees of Indiana University and
  * Indiana University Research and Technology Corporation.
  *
