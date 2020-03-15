77b02f84e1e32635c7455ad924056f54b3905ae1
Chip Simm <buddysimm@gmail.com>
2020-03-15 15:18:30 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,3 @@
-/*main.c ---------
- *
- * Filename: main.c
- * Description:
- * Author: Bryce Himebaugh
- * Maintainer:
- * Created: Thu Aug 18 16:32:20 2017
- * Last-Updated: September 23 15:38 2018
- *           By: Dmitrii Galantsev
- *
- */
-
-/* Commentary:
- *
- *
- *
- */
-
-/* Change log:
- *
- *
- */
-
 /* Copyright (c) 2004-2007 The Trustees of Indiana University and
  * Indiana University Research and Technology Corporation.
  *
